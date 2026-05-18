8136a69e14c6c235a2a06103f329df5376e23e83
ilya <iluhadgoker@gmail.com>
2026-05-18 21:56:15 +0300

    edit book_repo for getbyid

--- book_repo.go
+++ book_repo.go
@@ -9,8 +9,28 @@ import (
 
 type BookRepo interface {
 	GetById(ctx context.Context, id uuid.UUID) (*Book, error)
+	Create(ctx context.Context, b *Book) error
 }
 
 type bookRepo struct {
 	db *pgxpool.Pool
 }
+
+func NewBookRepo(db *pgxpool.Pool) BookRepo {
+	return &bookRepo{db: db}
+}
+
+func (r *bookRepo) GetById(ctx context.Context, id uuid.UUID) (*Book, error) {
+query := "SELECT id, title, author, year, price FROM books where id=$1"
+			b := &Book{} 
+			err := r.db.QueryRow(ctx, query, id).Scan(&b.ID, &b.Title, &b.Author, &b.Year, &b.Price)
+			if err != nil {
+				fmt.Errorf("get book by id", err)
+				http.Error(w, "book not found", http.StatusNotFound)
+				return nil, err
+			}
+			return b, nill
+}
+
+
+func (r *bookRepo) Create(ctx context.Context, id uuid.UUID) error {}
